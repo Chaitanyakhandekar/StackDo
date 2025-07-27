4d32c65d69a870b456b182d72f46116f3e76ec2b
Chaitanya <chaitanyakhandekar95@gmail.com>
2025-07-27 08:44:59 -0700

    done

--- Todo/src/main.jsx
+++ Todo/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
 import { BrowserRouter } from 'react-router'
-import { register } from '../public/serviceWorkerRegistration.js'  // 🆕 Import this
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -17,5 +17,4 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>
 )
 
-// Register the service worker here
-register()
+
